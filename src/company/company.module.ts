0fd36b010b32e4d8899778d01dc381a615cf4c1c
logan496 <filslogan87@gmail.com>
2026-08-08 00:16:29 +0100

    fix(company): fix of company relations (mail and commonModule)

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -6,9 +6,15 @@ import { CompanyController } from './company.controller';
 import { User } from '../user/user.entity';
 import { Role } from '../entity/role.entity';
 import { Country } from '../entity/country.entity';
+import { CommonModule } from '../common/common.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Company, User, Role, Country])],
+  imports: [
+    TypeOrmModule.forFeature([Company, User, Role, Country]),
+    CommonModule,
+    MailModule,
+  ],
   providers: [CompanyService],
   controllers: [CompanyController],
   exports: [CompanyService],
