857765aa86c0ad75a71619ad21d2837b94834ff0
logan496 <filslogan87@gmail.com>
2026-08-08 01:51:09 +0100

    fix(company): final fix for the insertion of module in database

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -4,10 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
 import { Company } from './company.entity';
+import { Country } from '../entity/country.entity';
 import { CommonModule } from '../common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Company]), CommonModule],
+  imports: [
+    TypeOrmModule.forFeature([Company, Country]),
+    CommonModule,
+  ],
   controllers: [CompanyController],
   providers: [CompanyService],
 })
