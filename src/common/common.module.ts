3173db9ae0c4c51f18a0eadd04e9734e18a75327
logan496 <filslogan87@gmail.com>
2026-08-08 01:02:38 +0100

    fix(company): fix module importations

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,19 @@
-import { ActivationTokenService } from './services/activation-token.service';
+// src/common/common.module.ts
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ActivationTokenService } from './services/activation-token.service';
+import { AccountCreationService } from './services/account-creation.service';
+import { User } from '../user/user.entity';
+import { Role } from '../entity/role.entity';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  providers: [ActivationTokenService /* autres services */],
-  exports: [ActivationTokenService], // ⚠️ ajoute cette ligne si manquante
+  imports: [TypeOrmModule.forFeature([User, Role]), MailModule],
+  providers: [
+    ActivationTokenService,
+    AccountCreationService /* autres services */,
+  ],
+  exports: [ActivationTokenService, AccountCreationService],
 })
 export class CommonModule {}
--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,22 +1,14 @@
+// company.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Company } from './company.entity';
-import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
-import { User } from '../user/user.entity';
-import { Role } from '../entity/role.entity';
-import { Country } from '../entity/country.entity';
+import { CompanyService } from './company.service';
+import { Company } from './company.entity';
 import { CommonModule } from '../common/common.module';
-import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Company, User, Role, Country]),
-    CommonModule,
-    MailModule,
-  ],
-  providers: [CompanyService],
+  imports: [TypeOrmModule.forFeature([Company]), CommonModule],
   controllers: [CompanyController],
-  exports: [CompanyService],
+  providers: [CompanyService],
 })
 export class CompanyModule {}
