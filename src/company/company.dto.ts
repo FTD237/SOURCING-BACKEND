246beefecd549ebb60781b4b2792b436d362db2b
logan496 <filslogan87@gmail.com>
2026-09-24 10:05:09 +0100

    refactor(company): ajout du champ localisation pour l'entreprise

--- src/company/company.dto.ts
+++ src/company/company.dto.ts
@@ -1,7 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsObject,
+} from 'class-validator';
 import { Company } from './company.entity';
 import { User } from '../user/user.entity';
+import type { LocalisationCompany } from '../common/types/localisation-company';
 
 export class CreateCompanyDto {
   // Informations User
@@ -24,6 +31,11 @@ export class CreateCompanyDto {
   @IsString()
   @IsNotEmpty()
   country_code: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsObject()
+  localisation?: LocalisationCompany;
 }
 
 export class UpdateCompanyDto {
@@ -41,6 +53,11 @@ export class UpdateCompanyDto {
   @IsString()
   @IsOptional()
   country_code?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsObject()
+  localisation?: LocalisationCompany;
 }
 
 export class CreateCompanyResponseDto {
